326cdfb17bdc7332c9b518d9d61761df030c9dff
camilosanchezdev <camilosanchezdev@gmail.com>
2020-05-30 19:35:21 -0300

    First update
    First update

---
+++ TicTacToe/TicTacToe-Winforms/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace TicTacToe_Winforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 35F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DimGray;
+            this.label1.Location = new System.Drawing.Point(173, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(323, 65);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Tic-Tac-Toe";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(601, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 38);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Score";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(583, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 25);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Player";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(683, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 25);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "PC";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(603, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 25);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(689, 193);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 25);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "00";
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Image = global::TicTacToe_Winforms.Properties.Resources.logo2;
+            this.pictureBox11.Location = new System.Drawing.Point(150, 12);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(346, 77);
+            this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox11.TabIndex = 25;
+            this.pictureBox11.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox9.Location = new System.Drawing.Point(374, 481);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(112, 112);
+            this.pictureBox9.TabIndex = 18;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox9_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox8.Location = new System.Drawing.Point(214, 481);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(112, 112);
+            this.pictureBox8.TabIndex = 17;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox7.Location = new System.Drawing.Point(57, 481);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(112, 112);
+            this.pictureBox7.TabIndex = 16;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox6.Location = new System.Drawing.Point(374, 318);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(112, 112);
+            this.pictureBox6.TabIndex = 15;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox5.Location = new System.Drawing.Point(214, 318);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(112, 112);
+            this.pictureBox5.TabIndex = 14;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox4.Location = new System.Drawing.Point(57, 318);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(112, 112);
+            this.pictureBox4.TabIndex = 13;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox3.Location = new System.Drawing.Point(374, 147);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(112, 112);
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Location = new System.Drawing.Point(214, 147);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(112, 112);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Location = new System.Drawing.Point(57, 147);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(112, 112);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Image = global::TicTacToe_Winforms.Properties.Resources.background_game;
+            this.pictureBox10.Location = new System.Drawing.Point(12, 112);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox10.TabIndex = 9;
+            this.pictureBox10.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(591, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 92);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "Play Again";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.CadetBlue;
+            this.label8.Location = new System.Drawing.Point(569, 498);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 38);
+            this.label8.TabIndex = 28;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(784, 661);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox10);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TicTacToe";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ TicTacToe/TicTacToe-Winforms/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe_Winforms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Image xbutton = Properties.Resources.x_button;
+        Image obutton = Properties.Resources.o_button;
+
+        List<System.Windows.Forms.PictureBox> blanks = new List<System.Windows.Forms.PictureBox>();
+        public void AddBox()
+        {
+            blanks.Add(pictureBox1);
+            blanks.Add(pictureBox2);
+            blanks.Add(pictureBox3);
+            blanks.Add(pictureBox4);
+            blanks.Add(pictureBox5);
+            blanks.Add(pictureBox6);
+            blanks.Add(pictureBox7);
+            blanks.Add(pictureBox8);
+            blanks.Add(pictureBox9);
+        }
+        Random rnd = new Random();
+        int clicks = 5;
+        int playerScore = 0;
+        int pcScore = 0;
+        bool win = false;
+        public void pcChoice()
+        {
+            
+            int numeroRandom = rnd.Next(0, 9);
+            while ((blanks[numeroRandom].Image == obutton || blanks[numeroRandom].Image == xbutton) && clicks > 0)
+            {
+                numeroRandom = rnd.Next(0, 9);
+            }
+            if (clicks != 0)
+            {
+                blanks[numeroRandom].Image = obutton;
+                blanks[numeroRandom].Enabled = false;
+                // check row PC
+
+                for (int i = 0; i < blanks.Count; i += 3)
+                {
+                    if (blanks[i].Image == obutton && blanks[i + 1].Image == obutton && blanks[i + 2].Image == obutton)
+                    {
+                        label8.Text = "PC Wins";
+                        ButtonsActive(false);
+                        pcScore++;
+                        win = true;
+                        clicks = 0;
+                        return;
+                    }
+                }
+                //  check column PC
+                for (int i = 0; i < 3; i++)
+                {
+                    if (blanks[i].Image == obutton && blanks[i + 3].Image == obutton && blanks[i + 6].Image == obutton)
+                    {
+                        label8.Text = "PC Wins";
+                        ButtonsActive(false);
+                        pcScore++;
+                        win = true;
+                        clicks = 0;
+                        return;
+                    }
+                }
+                // check diagonal PC
+                if (blanks[0].Image == obutton && blanks[4].Image == obutton && blanks[8].Image == obutton)
+                {
+                    label8.Text = "PC Wins";
+                    ButtonsActive(false);
+                    pcScore++;
+                    win = true;
+                    clicks = 0;
+                    return;
+                }
+                if (blanks[2].Image == obutton && blanks[4].Image == obutton && blanks[6].Image == obutton)
+                {
+                    label8.Text = "PC Wins";
+                    ButtonsActive(false);
+                    pcScore++;
+                    win = true;
+                    clicks = 0;
+                    return;
+                }
+            }
+        }
+        public void Check()
+        {
+            AddBox();
+            // check row player
+            for (int i = 0; i < blanks.Count; i += 3)
+            {
+                if (blanks[i].Image == xbutton && blanks[i + 1].Image == xbutton && blanks[i + 2].Image == xbutton)
+                {
+                    label8.Text = "Player Wins";
+                    ButtonsActive(false);
+                    playerScore++;
+                    win = true;
+                    clicks = 0;
+                    return;
+                }
+            }
+            // check column player
+            for (int i = 0; i < 3; i ++)
+            {
+                if (blanks[i].Image == xbutton && blanks[i + 3].Image == xbutton && blanks[i + 6].Image == xbutton)
+                {
+                    label8.Text = "Player Wins";
+                    ButtonsActive(false);
+                    playerScore++;
+                    win = true;
+                    clicks = 0;
+                    return; 
+                }
+            }
+            // check diagonal player
+            if (blanks[0].Image == xbutton && blanks[4].Image == xbutton && blanks[8].Image == xbutton)
+            {
+                label8.Text = "Player Wins";
+                ButtonsActive(false);
+                playerScore++;
+                win = true;
+                clicks = 0;
+                return;
+            }
+            if (blanks[2].Image == xbutton && blanks[4].Image == xbutton && blanks[6].Image == xbutton)
+            {
+                label8.Text = "Player Wins";
+                ButtonsActive(false);
+                playerScore++;
+                win = true;
+                clicks = 0;
+                return;
+            }
+            if (clicks == 0)
+            {
+                label8.Text = "Tie";
+                ButtonsActive(false);
+            }
+        }
+        public void Score()
+        {
+            if (win)
+            {
+                label5.Text = playerScore.ToString("00");
+                label6.Text = pcScore.ToString("00");
+            }
+        }
+        public void ButtonsActive(bool x)
+        {
+            pictureBox1.Enabled = x;
+            pictureBox2.Enabled = x;
+            pictureBox3.Enabled = x;
+            pictureBox4.Enabled = x;
+            pictureBox5.Enabled = x;
+            pictureBox6.Enabled = x;
+            pictureBox7.Enabled = x;
+            pictureBox8.Enabled = x;
+            pictureBox9.Enabled = x;
+        }
+        public void Functions()
+        {
+            Check();
+            pcChoice();
+            Score();
+        }
+        // Spaces
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = xbutton;
+            pictureBox1.Enabled = false;
+            clicks--;
+            Functions();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            pictureBox3.Image = xbutton;
+            pictureBox3.Enabled = false;
+            clicks--;
+            Functions();
+        }
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            pictureBox4.Image = xbutton;
+            pictureBox4.Enabled = false;
+            clicks--;
+            Functions();
+        }
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            pictureBox5.Image = xbutton;
+            pictureBox5.Enabled = false;
+            clicks--;
+            Functions();
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = xbutton;
+            pictureBox2.Enabled = false;
+            clicks--;
+            Functions();
+        }
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            pictureBox6.Image = xbutton;
+            pictureBox6.Enabled = false;
+            clicks--;
+            Functions();
+        }
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            pictureBox7.Image = xbutton;
+            pictureBox7.Enabled = false;
+            clicks--;
+            Functions();
+        }
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            pictureBox8.Image = xbutton;
+            pictureBox8.Enabled = false;
+            clicks--;
+            Functions();
+        }
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            pictureBox9.Image = xbutton;
+            pictureBox9.Enabled = false;
+            clicks--;
+            Functions();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Reset
+            ButtonsActive(true);
+            foreach (var item in blanks)
+            {
+                item.Image = null;
+            }
+            clicks = 5;
+            label8.Text = "";
+        }
+    }
+}
